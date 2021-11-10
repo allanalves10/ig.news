26f7aaf831c1ee11f57b84ee644237d97df71c85
Allan Alves <allanjunior1997@gmail.com>
2021-11-10 17:16:36 -0300

    Finalização do layout da aplicação

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,26 @@
 import Head from 'next/head';
+import { SubscribeButton } from '../components/SubscribeButton';
+import styles from './home.module.scss';
 
 export default function Home() {
   return (
     <>
       <Head>
-        <title>Início | Ig.News</title>
+        <title>Home | Ig.News</title>
       </Head>
-      <h1>
-        ...
-      </h1>
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏 Hey, welcome!</span>
+          <h1>news about the <span>React</span> world.</h1>
+          <p>
+            Get access to all the publications <br />
+            <span>for $9.90 month</span>
+          </p>
+
+          <SubscribeButton />
+        </section>
+        <img src="/images/avatar.svg" alt="girl coding" />
+      </main>
     </>
   )
 }
